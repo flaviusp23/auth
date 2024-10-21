c44dd40fbc43ed0ca18b2f59ae04ecea52cf69b6
flaviusp23 <flavius.paltin@yahoo.com>
2024-10-21 15:50:22 +0300

    reset forgot done

--- server/utils/index.js
+++ server/utils/index.js
@@ -2,7 +2,8 @@ const { createJWT, isTokenValid, attachCookiesToResponse } = require("./jwt");
 const createTokenUser = require("./createTokenUser");
 const checkPermissions = require("./checkPermissions");
 const sendVerificationEmail = require("./sendVerificationEmail");
-const sendResetPasswordEmail = require("./sendResetPasswordEmail");
+const sendResetPassswordEmail = require("./sendResetPasswordEmail");
+const createHash = require("./createHash");
 module.exports = {
   createJWT,
   isTokenValid,
@@ -10,5 +11,6 @@ module.exports = {
   createTokenUser,
   checkPermissions,
   sendVerificationEmail,
-  sendResetPasswordEmail,
+  sendResetPassswordEmail,
+  createHash,
 };
